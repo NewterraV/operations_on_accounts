e4d47fb32c7e1f57bb894c48e55790a2d604aa78
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-01 09:36:21 +0400

    Extended functionality of the class. Written tests for new features.

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -29,7 +29,7 @@ data_2 = {
         }
     },
     "description": "Перевод организации",
-    "from": "Счет 27248529432547658655",
+    "from": "",
     "to": "Счет 97584898735659638967"
 }
 
@@ -44,21 +44,34 @@ data_3 = {
             "code": "RUB"
         }
     },
+    "to": "Счет 97584898735659638967"
+}
+
+data_4 = {
+    "id": 649467725,
+    "state": "EXECUTED",
+    "date": "2018-04-14T19:35:28.978265",
+    "operationAmount": {
+        "amount": "96995.73",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    },
+    "description": "",
+    "from": "Счет 97584898735659638967",
+    "to": ""
 }
 
 
-@pytest.mark.parametrize('item, result', [(data,
-                                           {"date": "26.08.2019",
-                                            "from": "Maestro 1596837868705199",
-                                            "to": "Счет 64686473678894779589",
-                                            "description": "Перевод организации",
-                                            "amount": "31957.58 руб."}),
-                                          (data_2, {}),
-                                          (data_3, {"date": "26.08.2019",
-                                                    "from": "",
-                                                    "to": "",
-                                                    "description": "",
-                                                    "amount": "31957.58 руб."})])
-def test_get_dict(item, result):
-    instance = Operation(item)
-    assert instance.get_dict() == result
+@pytest.mark.parametrize('item, result', [
+    (data, f'26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет ** 9589\n31957.58 руб.\n'),
+    (data_3, f'26.08.2019\n-> Счет ** 8967\n31957.58 руб.\n'),
+    (data_4, f'14.04.2018\nСчет ** 8967 ->\n96995.73 руб.\n')])
+def test_get_text(item, result):
+    assert Operation(item).get_text() == result
+
+
+@pytest.mark.parametrize('item, result', [(data, True), (data_2, False)])
+def test_check_state(item, result):
+    assert Operation(item).check_state() == result

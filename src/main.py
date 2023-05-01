4b55f7db185e58905f7540926653104482faf893
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-01 12:26:44 +0400

    Fixed a bug in issuing incorrect operations due to ignoring the date.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+import json
+
 from src.cnst import PATH_OPERATIONS
-from src.utils import read_json, get_text_operation
+from src.utils import read_json, get_text_operation, get_date
 from src.classes.operation import Operation
 
 
 def main(path):
-    list_operations = list(reversed(read_json(path)))
+    list_json = read_json(path)
+    list_json.remove({})
+    list_operations = sorted(list_json, key=get_date, reverse=True)
     list_text = []
 
     for i in list_operations:
